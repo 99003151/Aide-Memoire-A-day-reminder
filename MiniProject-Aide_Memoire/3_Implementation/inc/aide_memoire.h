12a07ceb1ed355c369c389cf449b7b082b53c1f8
Aiswarya Sasikumar <69103516+stepin104959@users.noreply.github.com>
2020-10-08 11:52:01 +0530

    Create aide_memoire.h

---
+++ MiniProject-Aide_Memoire/3_Implementation/inc/aide_memoire.h
@@ -0,0 +1,18 @@
+/** 
+* @file aide-memoire.h
+* Leap year 
+*/
+#ifndef __AIDE-MEMOIRE_H__
+#define __AIDE-MEMOIRE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  adds the operand1 and operand2 and returns the result
+* @param[in] 
+* @return Result of operand1 + operand2
+*/
+int add(int operand1, int operand2);
+
